736e2ad1a63fdc7f231f88ecae22ebf5fa36988f
altoPancho <altopancho.general@gmail.com>
2022-06-30 08:40:38 -0300

    html/css -> components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,29 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { EducationItemComponent } from './components/education-item/education-item.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ExperienceItemComponent } from './components/experience-item/experience-item.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ProjectItemComponent } from './components/project-item/project-item.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    ProfileComponent
+    ProfileComponent,
+    SkillsComponent,
+    EducationComponent,
+    EducationItemComponent,
+    ExperienceComponent,
+    ExperienceItemComponent,
+    ProjectsComponent,
+    ProjectItemComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
