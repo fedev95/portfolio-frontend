09c1832f5f34a9e975cabe10fe8c877827b09b85
Federico Burgos <federicoburgos.dev@gmail.com>
2022-08-17 02:58:10 -0300

    listo loginComponent.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/model/login.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
 	selector: 'app-login',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-	constructor() { 
+	isLogged = false;
+	isLoginFail = false;
+	loginUsuario!: Login;
+	nombreUsuario!: string;
+	password!: string;
+	roles: string[] = [];
+	errorMsj!: string;
+
+	constructor(private tokenService: TokenService, private authService: AuthService, private router: Router) { 
   	}
 
   	ngOnInit() {
+		if (this.tokenService.getToken()) {
+			this.isLogged = true;
+			this.isLoginFail = false;
+			this.roles = this.tokenService.getAuthorities();
+		}
+	}
+
+	onLogin(): void {
+		this.loginUsuario = new Login(this.nombreUsuario, this.password);
+		this.authService.login(this.loginUsuario).subscribe(data => {
+			this.isLogged = true;
+			this.isLoginFail = false;
+			this.tokenService.setToken(data.token);
+			this.tokenService.setUserName(data.nombreUsuario);
+			this.tokenService.setAuthorities(data.authorities);
+			this.roles = data.authorities;
+			this.router.navigate(['']);
+		}, err => {
+			this.isLogged = false;
+			this.isLoginFail = true;
+			this.errorMsj = err.error.mensaje;
+			
+		})
 	}
 }
