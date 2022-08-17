915cd0ccf3b976823e26dcff6209156d5f90ce9f
Federico Burgos <federicoburgos.dev@gmail.com>
2022-08-17 02:27:53 -0300

    borrado ts de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
 	selector: 'app-login',
@@ -10,42 +7,9 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-	user = '';
-	password = '';
-
-	loginForm: FormGroup;
-
-  	constructor(private formBuilder: FormBuilder, private authService: AuthService, private route:Router){ 
-    	this.loginForm= this.formBuilder.group({
-			user:['', [Validators.required]],
-      		password:['',[Validators.required]],
-   		})
+	constructor() { 
   	}
 
-  	ngOnInit() {}
-
-	get Password(){
-		return this.loginForm.get("password");
+  	ngOnInit() {
 	}
-	
-	get User(){
-		return this.loginForm.get("user");
-	}
-
-	get PasswordValid(){
-		return this.Password?.touched && !this.Password?.valid;
-	}
-
-	get UserValid() {
-		return false;
-	}
- 
-	onSubmit(event: Event) {
-		event.preventDefault; 
-		
-		this.authService.logIn(this.loginForm.value).subscribe(data => {
-			this.route.navigate(['/portfolio']);
-		})
-	}	
-
 }
