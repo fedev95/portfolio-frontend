5c3efa0940ee7da0c04462f2d37f164b031d5bd1
Federico Burgos <federicoburgos.dev@gmail.com>
2022-08-17 02:39:09 -0300

    listo authService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JwtDto } from '../model/jwt-dto';
 import { Login } from '../model/login.model';
 
 @Injectable({
@@ -7,12 +9,13 @@ import { Login } from '../model/login.model';
 })
 export class AuthService {
 
-	authURL = 'http://localhost:8080/auth/';
+	authURL = 'http://localhost:8080/auth';
 
 	constructor(private httclient: HttpClient) {
 	}
 
-	public login(login: Login) {
-		
+	public login(login: Login): Observable<JwtDto> {
+		return this.httclient.post<JwtDto>(this.authURL + '/login', login);
 	}
+
 }
